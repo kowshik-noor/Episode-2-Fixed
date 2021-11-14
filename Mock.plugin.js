a185ea6f798a2332b9df9f93f2c24b6fd3cd6361
Archer2305 <73808514+Archer2305@users.noreply.github.com>
2021-11-13 20:32:42 -0800

    Update Mock.plugin.js

--- Mock.plugin.js
+++ Mock.plugin.js
@@ -14,14 +14,14 @@
     start() {
 
 
-setInterval(function() {
+setInterval(function() { //set the code to repeat without pausing discord itself.
    var a= document.querySelector("span[data-slate-string]");
     if(a!=null||a!=undefined){
        var UserTyping= document.querySelector("span[data-slate-string]").textContent 
-    
+    //get what the user is typing 
 
-if(UserTyping.startsWith("|mock")&& UserTyping.charAt(UserTyping.length-1)=="|" ){
-var cache=[UserTyping.length-1]
+if(UserTyping.startsWith("|mock")&& UserTyping.charAt(UserTyping.length-1)=="|" ){ //check if the user has typed the proper syntax 
+var cache=[UserTyping.length-1] // convert into ThE mOcK cAsE
 let NewMessage=UserTyping;
 NewMessage=NewMessage.replace("|mock ","")
 NewMessage=NewMessage.replace("|","")
@@ -32,10 +32,10 @@ cache[i]=NewMessage.charAt(i).toLocaleUpperCase()
 }
 NewMessage=cache.join("");
 
- document.querySelector("span[data-slate-string]").textContent=NewMessage
+ document.querySelector("span[data-slate-string]").textContent=NewMessage // set the text bar to the new string 
 }
     }
-},300);
+},300);// repeat every 300 miliseconds 
 
 
 }
